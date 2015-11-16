61924c2a68b131044a174f74d73c1713b3ee267b
David Tran <isorunner@yahoo.com>
2015-11-15 18:40:39 -0800

    Add validations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,8 @@
 class Pin < ActiveRecord::Base
   acts_as_votable
   belongs_to :user
+  validates :description, presence: true
+  validates :title, presence: true
   has_attached_file :image, styles: { medium: "300x300>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
